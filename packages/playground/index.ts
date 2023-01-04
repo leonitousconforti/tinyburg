66679bc4bb811cb124abccc838feb0f5c86afb3b
Leo Conforti <leo@leoconforti.us>
2023-01-04 22:11:00 +0000

    mess around in playground

--- packages/playground/index.ts
+++ packages/playground/index.ts
@@ -1,2 +1,22 @@
-import {} from "@tinyburg/architect";
-import { bootstrapAgentOnRemote } from "@tinyburg/insight";
+import { loadFromApkpure } from "@tinyburg/apks";
+import { Emulator, LocalEmulator } from "@tinyburg/architect";
+
+// Create emulator instance
+const emulator: Emulator = new LocalEmulator("testing");
+await emulator.create();
+
+// Launch emulator and install TinyTower version 4.12.0
+const { installApk, launchGame, stopGame } = await emulator.up();
+const apk = loadFromApkpure("4.12.0");
+await installApk(apk);
+
+// Launch TinyTower
+const { processPid } = await launchGame();
+console.log(`TinyTower running with pid: ${processPid}`);
+
+// Stop TinyTower after 1 minute
+setTimeout(async () => {
+    await stopGame();
+    await emulator.down();
+    await emulator.destroy();
+});
