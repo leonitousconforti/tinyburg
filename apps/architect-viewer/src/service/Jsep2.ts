d41a40d82611b532d9d0944bf2a0c75fcfd6ef07
Leo Conforti <leo@leoconforti.us>
2023-02-24 18:03:41 +0000

    still working

--- apps/architect-viewer/src/service/Jsep2.ts
+++ apps/architect-viewer/src/service/Jsep2.ts
@@ -5,62 +5,76 @@ import { EventEmitter } from "events";
 import { JsepMsg } from "../generated/rtc_service";
 
 export default class JsepProtocol extends EventEmitter {
-    private rtcId: RtcId | undefined;
     private readonly rtcClient: IRtcClient;
+
+    // private haveOffer: boolean;
+    private candidates: RTCIceCandidateInit[];
     private peerConnection: RTCPeerConnection | undefined;
-    private old_emu_patch: {
-        candidates: unknown[];
-        sdp: unknown;
-        haveOffer: boolean;
-        answer: boolean;
-    };
 
     constructor(rtcClient: IRtcClient) {
         super();
-
-        this.rtcId = undefined;
         this.rtcClient = rtcClient;
-        this.peerConnection = undefined;
 
-        this.old_emu_patch = {
-            candidates: [],
-            sdp: null,
-            haveOffer: false,
-            answer: false,
-        };
+        this.candidates = [];
+        // this.haveOffer = false;
+        this.peerConnection = undefined;
     }
 
     disconnect = () => {
-        if (this.peerConnection) this.peerConnection.close();
-        this.old_emu_patch = {
-            candidates: [],
-            sdp: null,
-            haveOffer: false,
-            answer: false,
-        };
-        this.emit("disconnected", this);
+        this.candidates = [];
+        // this.haveOffer = false;
+        this.peerConnection?.close();
+        this.emit("disconnected");
     };
 
     startStream = async () => {
-        this.rtcId = await this.rtcClient.requestRtcStream({}).response;
-        const stream = this.rtcClient.receiveJsepMessages(this.rtcId);
-        for await (const response of stream.responses) {
-            const msg = response.message;
-            const signal = JSON.parse(msg);
-            this._handleSignal(signal);
+        const rtcId: RtcId = await this.rtcClient.requestRtcStream({}).response;
+        const jsepStream = this.rtcClient.receiveJsepMessages(rtcId);
+
+        for await (const response of jsepStream.responses) {
+            const signal = JSON.parse(response.message);
+
+            if (signal.start) {
+                this.peerConnection = new RTCPeerConnection(signal.start);
+                this.peerConnection.ontrack = this.handlePeerConnectionTrack;
+                this.peerConnection.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
+                    if (event.candidate === null) return;
+                    this.sendJsep(rtcId, { candidate: event.candidate });
+                };
+                this.peerConnection.onsignalingstatechange = this._handlePeerState;
+                this.peerConnection.onconnectionstatechange = this._handlePeerConnectionStateChange;
+            }
+
+            if (signal.bye) {
+                this.disconnect();
+            }
+
+            if (signal.sdp) {
+                this.peerConnection?.setRemoteDescription(
+                    new RTCSessionDescription(signal as RTCSessionDescriptionInit)
+                );
+                const answer = await this.peerConnection?.createAnswer();
+                this.peerConnection?.setLocalDescription(answer);
+                this.sendJsep(rtcId, { sdp: answer });
+            }
+
+            if (signal.candidate) {
+                this.candidates.push(signal);
+            }
+
+            // if (this.peerConnection) {
+            //     if (this.haveOffer) {
+            //         console.log("here!");
+            //         while (this.candidates.length > 0) {
+            //             console.log("Here!!");
+            //             this.handleCandidate(this.candidates.shift()!);
+            //         }
+            //     }
+            // }
         }
     };
 
-    cleanup = () => {
-        this.disconnect();
-        if (this.peerConnection) {
-            this.peerConnection.removeEventListener("track", this._handlePeerConnectionTrack);
-            this.peerConnection.removeEventListener("icecandidate", this._handlePeerIceCandidate);
-            this.peerConnection = undefined;
-        }
-    };
-
-    _handlePeerConnectionTrack = (event: RTCTrackEvent) => {
+    private handlePeerConnectionTrack = (event: RTCTrackEvent) => {
         this.emit("connected", event.track);
     };
 
@@ -73,88 +87,27 @@ export default class JsepProtocol extends EventEmitter {
         }
     };
 
-    _handlePeerIceCandidate = (event: RTCPeerConnectionIceEvent) => {
-        if (event.candidate === null) return;
-        this._sendJsep({ candidate: event.candidate });
-    };
-
-    _handleStart = (signal: any) => {
-        this.peerConnection = new RTCPeerConnection(signal.start);
-        this.peerConnection.ontrack = this._handlePeerConnectionTrack;
-        this.peerConnection.onicecandidate = this._handlePeerIceCandidate;
-        this.peerConnection.onconnectionstatechange = this._handlePeerConnectionStateChange;
-        this.peerConnection.onsignalingstatechange = this._handlePeerState;
-    };
-
     _handlePeerState = () => {
         if (!this.peerConnection) {
             console.log("Peerconnection no longer available, ignoring signal state.");
         }
         switch (this.peerConnection?.signalingState) {
             case "have-remote-offer":
-                this.old_emu_patch.haveOffer = true;
-                while (this.old_emu_patch.candidates.length > 0) {
-                    this._handleCandidate(this.old_emu_patch.candidates.shift());
+                // this.haveOffer = true;
+                while (this.candidates.length > 0) {
+                    this.handleCandidate(this.candidates.shift()!);
                 }
                 break;
         }
     };
 
-    _handleSDP = async (signal: any) => {
-        this.old_emu_patch.sdp = null;
-        this.peerConnection?.setRemoteDescription(new RTCSessionDescription(signal));
-        const answer = await this.peerConnection?.createAnswer();
-        if (answer) {
-            if (!this.old_emu_patch.answer) {
-                this.old_emu_patch.answer = true;
-                this.peerConnection?.setLocalDescription(answer);
-                this._sendJsep({ sdp: answer });
-            }
-        } else {
-            this.disconnect();
-        }
+    private handleCandidate = (candidateInitDict?: RTCIceCandidateInit) => {
+        this.peerConnection?.addIceCandidate(new RTCIceCandidate(candidateInitDict));
     };
 
-    _handleCandidate = (signal: any) => {
-        this.peerConnection?.addIceCandidate(new RTCIceCandidate(signal));
-    };
-
-    _handleSignal = (signal: any) => {
-        console.log(signal);
-        if (signal.start) {
-            this._handleStart(signal);
-        }
-        if (signal.bye) {
-            this._handleBye();
-        }
-        if (signal.sdp && !this.old_emu_patch.sdp) {
-            this.old_emu_patch.sdp = signal;
-        }
-        if (signal.candidate) {
-            this.old_emu_patch.candidates.push(signal);
-        }
-
-        if (!!this.peerConnection) {
-            // We have created a peer connection..
-            if (this.old_emu_patch.sdp) {
-                this._handleSDP(this.old_emu_patch.sdp);
-            }
-
-            if (this.old_emu_patch.haveOffer) {
-                while (this.old_emu_patch.candidates.length > 0) {
-                    this._handleCandidate(this.old_emu_patch.candidates.shift());
-                }
-            }
-        }
-    };
-
-    _handleBye = () => {
-        this.disconnect();
-    };
-
-    _sendJsep = async (jsonObject: any) => {
+    private sendJsep = async (rtcId: RtcId, jsonObject: any) => {
         const request = JsepMsg.create();
-        request.id = this.rtcId!;
+        request.id = rtcId;
         request.message = JSON.stringify(jsonObject);
         await this.rtcClient.sendJsepMessage(request);
     };
