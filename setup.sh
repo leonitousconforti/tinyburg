306966f3c50e5f18496443e4b9d2a5518e665c7a
Leo Conforti <leo@leoconforti.us>
2023-05-15 23:11:13 +0000

    add setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
+#!/usr/bin/env bash
+
+echo "🚀 Setting up tinyburg devcontainer..."
+
+# Install rush and pnpm package manager
+echo "📦 Installing Rush and Pnpm dependencies..."
 npm install -g pnpm @microsoft/rush
-rush update
+
+# Install tinyburg dependencies
+echo "📦 Installing monorepo dependencies..."
+rush install
 rush update-autoinstaller --name rush-prettier
+
+echo "🚀 Devcontainer setup complete!"
+echo "🙏 Thank you for contributing to Tinyburg!"
