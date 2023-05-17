79b316b72671824e4ccc94677de4858e2b62e09c
Leo Conforti <leo@leoconforti.us>
2023-05-17 03:45:03 +0000

    update architect

--- packages/architect/infrastructure/droplets.tf
+++ packages/architect/infrastructure/droplets.tf
@@ -7,18 +7,12 @@ resource "digitalocean_project_resources" "myproject_resources" {
     resources = [digitalocean_droplet.aosp-emu.urn]
 }
 
-resource "digitalocean_ssh_key" "my_ssh_key" {
-    name = "macos-laptop"
-    public_key = var.ssh_pub_key
-}
-
 resource "digitalocean_droplet" "aosp-emu" {
     region = "sfo3"
     size   = "s-1vcpu-2gb"
     image  = "ubuntu-22-10-x64"
     name   = "aosp-emu-1vcpu-2gb"
     tags = ["prod", "android-emulator"]
-    ssh_keys = [digitalocean_ssh_key.my_ssh_key.id]
     ipv6 = true
     monitoring = true
     droplet_agent = true
--- packages/architect/infrastructure/variables.tf
+++ packages/architect/infrastructure/variables.tf
@@ -1,4 +1,3 @@
 variable "do_token" {}
-variable "ssh_pub_key" {}
 variable "domain_name" {}
 variable "do_project_name" {}
