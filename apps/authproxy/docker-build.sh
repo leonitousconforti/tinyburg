e1328448c2c28e831eda624eff530ea320f5542b
leo conforti <leo@leoconforti.us>
2023-06-12 18:45:58 +0000

    add git commit hash as build arg

--- apps/authproxy/docker-build.sh
+++ apps/authproxy/docker-build.sh
@@ -3,4 +3,4 @@ set -e
 
 rush build --to @tinyburg/authproxy
 rush deploy --project @tinyburg/authproxy --create-archive /workspaces/tinyburg/apps/authproxy/deploy.zip --create-archive-only
-docker build -t ghcr.io/leonitousconforti/tinyburg/authproxy:latest .
+docker build --build-arg GIT_SHA=$(git rev-parse --short HEAD) -t ghcr.io/leonitousconforti/tinyburg/authproxy:latest .
