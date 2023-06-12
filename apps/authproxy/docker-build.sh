f340e6c8dc3b5e56cf6c82c857b065763ec09cf2
leo conforti <leo@leoconforti.us>
2023-06-12 18:34:18 +0000

    update docker build and prepare to publish

--- apps/authproxy/docker-build.sh
+++ apps/authproxy/docker-build.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+rush build --to @tinyburg/authproxy
 rush deploy --project @tinyburg/authproxy --create-archive /workspaces/tinyburg/apps/authproxy/deploy.zip --create-archive-only
-docker build -t tinyburg/authproxy:latest .
+docker build -t ghcr.io/leonitousconforti/tinyburg/authproxy:latest .
