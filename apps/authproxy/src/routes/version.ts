aaaf05cce08a09bdf87f59c6414232361d586538
leo conforti <leo@leoconforti.us>
2023-06-12 18:57:19 +0000

    finally fix version route

--- apps/authproxy/src/routes/version.ts
+++ apps/authproxy/src/routes/version.ts
@@ -6,12 +6,8 @@ const version: FastifyPluginAsync = async (fastify): Promise<void> => {
         method: ["GET"],
 
         // eslint-disable-next-line @typescript-eslint/naming-convention
-        handler: async (_request, reply) => {
-            await reply.status(200);
-            reply.raw.write(`Running in NODE_ENV -> ${process.env["NODE_ENV"] || "unknown"}\n`);
-            reply.raw.write(`Running from GIT_SHA -> ${process.env["GIT_SHA"] || "unknown"}`);
-            reply.raw.end();
-        },
+        handler: (_request, _reply) =>
+            `Running in NODE_ENV -> ${process.env["NODE_ENV"]}\nRunning from GIT_SHA -> ${process.env["GIT_SHA"]}`,
     });
 };
 
