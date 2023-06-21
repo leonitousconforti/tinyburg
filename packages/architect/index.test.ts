bc8e86942fd10e9ff25cec9f6175e18c0659a24a
Leo Conforti <leo@leoconforti.us>
2023-06-21 18:17:07 +0000

    test

---
+++ packages/architect/index.test.ts
@@ -0,0 +1,8 @@
+import architect from "./index.js";
+import loadApk from "@tinyburg/apks";
+
+describe("simple testing", () => {
+    it("Should be true", async () => {
+        expect(true).toBeTruthy();
+    });
+});
