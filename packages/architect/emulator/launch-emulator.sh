81f6cd25fd235212d84158143fe440dde19fbb76
Leo Conforti <leo@leoconforti.us>
2023-06-21 21:33:44 +0000

    do not wait for emulator

--- packages/architect/emulator/launch-emulator.sh
+++ packages/architect/emulator/launch-emulator.sh
@@ -13,15 +13,6 @@ pulseaudio -D --log-time=1 --log-target=newfile:/tmp/pulseverbose.log --log-time
 /android/sdk/platform-tools/adb start-server
 /android/sdk/emulator/emulator -avd Pixel2 -no-window -ports 5554,5555 -grpc 8554 -gpu swiftshader_indirect -qemu -append panic=1 &
 
-# https://android.stackexchange.com/questions/83726/how-to-adb-wait-for-device-until-the-home-screen-shows-up
-/android/sdk/platform-tools/adb wait-for-device
-A=$(/android/sdk/platform-tools/adb shell getprop sys.boot_completed | tr -d '\r')
-while [ "$A" != "1" ]; do
-    sleep 2s
-    A=$(/android/sdk/platform-tools/adb shell getprop sys.boot_completed | tr -d '\r')
-done
-sleep 30s
-
 # Listeners, needed because adb does not bind to all interfaces
 # so just forwarding the port through docker does not work
 socat tcp-listen:27042,reuseaddr,fork tcp:127.0.0.1:27043 &
