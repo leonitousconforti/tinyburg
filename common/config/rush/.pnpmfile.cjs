f4f416f0b0efc558e9c5ab069baeadaf136c7fcd
Leo Conforti <leo@leoconforti.us>
2023-06-23 03:16:08 +0000

    update a bunch of rush configs

--- common/config/rush/.pnpmfile.cjs
+++ common/config/rush/.pnpmfile.cjs
@@ -2,15 +2,16 @@
 
 /**
  * When using the PNPM package manager, you can use pnpmfile.js to workaround
- * dependencies that have mistakes in their package.json file.  (This feature is
+ * dependencies that have mistakes in their package.json file. (This feature is
  * functionally similar to Yarn's "resolutions".)
  *
  * For details, see the PNPM documentation:
  * https://pnpm.js.org/docs/en/hooks.html
  *
- * IMPORTANT: SINCE THIS FILE CONTAINS EXECUTABLE CODE, MODIFYING IT IS LIKELY TO INVALIDATE
- * ANY CACHED DEPENDENCY ANALYSIS.  After any modification to pnpmfile.js, it's recommended to run
- * "rush update --full" so that PNPM will recalculate all version selections.
+ * IMPORTANT: SINCE THIS FILE CONTAINS EXECUTABLE CODE, MODIFYING IT IS LIKELY
+ * TO INVALIDATE ANY CACHED DEPENDENCY ANALYSIS. After any modification to
+ * pnpmfile.js, it's recommended to run "rush update --full" so that PNPM will
+ * recalculate all version selections.
  */
 module.exports = {
     hooks: {
@@ -19,12 +20,10 @@ module.exports = {
 };
 
 /**
- * This hook is invoked during installation before a package's dependencies
- * are selected.
- * The `packageJson` parameter is the deserialized package.json
- * contents for the package that is about to be installed.
- * The `context` parameter provides a log() function.
- * The return value is the updated object.
+ * This hook is invoked during installation before a package's dependencies are
+ * selected. The `packageJson` parameter is the deserialized package.json
+ * contents for the package that is about to be installed. The `context`
+ * parameter provides a log() function. The return value is the updated object.
  */
 function readPackage(packageJson, context) {
     // // The karma types have a missing dependency on typings from the log4js package.
