e7c8dd5ffc5838c3839129ef323cb6a73f7e718a
Leo Conforti <leo@leoconforti.us>
2023-06-26 16:52:25 +0000

    testing for architect

--- packages/architect/src/index.test.ts
+++ packages/architect/src/index.test.ts
@@ -1,10 +1,11 @@
 /* eslint-disable dot-notation */
 
+import frida from "frida";
 import Dockerode from "dockerode";
 import architect from "./index.js";
 
-// Timeout tests after 10 minutes and cleanup after 1 minute
-const ARCHITECT_TEST_TIMEOUT_MS = Number.parseInt(process.env["ARCHITECT_TEST_TIMEOUT_MS"] as string) || 1000 * 60 * 10;
+// Timeout tests after 3 minutes and cleanup after 1 minute
+const ARCHITECT_TEST_TIMEOUT_MS = Number.parseInt(process.env["ARCHITECT_TEST_TIMEOUT_MS"] as string) || 1000 * 60 * 3;
 const ARCHITECT_CLEANUP_TIMEOUT_MS = Number.parseInt(process.env["ARCHITECT_CLEANUP_TIMEOUT_MS"] as string) || 60_000;
 
 // Override the docker host environment variable just for these tests
@@ -14,13 +15,9 @@ describe("simple tests", () => {
     const dockerode: Dockerode = new Dockerode();
 
     const cleanUpArchitectArtifacts = async (): Promise<void> => {
-        const allImages = await dockerode.listImages({ all: true });
         const allNetworks = await dockerode.listNetworks({ all: true });
         const allContainers = await dockerode.listContainers({ all: true });
 
-        const allArchitectImages = allImages
-            .filter((image) => image.RepoTags?.[0]?.includes("architect"))
-            .map((image) => dockerode.getImage(image.Id));
         const allArchitectNetworks = allNetworks
             .filter((network) => network.Name.startsWith("ghcr.io/leonitousconforti/tinyburg/architect:emulator-"))
             .map((network) => dockerode.getNetwork(network.Id));
@@ -34,7 +31,6 @@ describe("simple tests", () => {
 
         await Promise.all(allRunningArchitectContainers.map((container) => container.stop()));
         await Promise.all(allArchitectContainers.map((container) => container.remove()));
-        await Promise.all(allArchitectImages.map((image) => image.remove()));
         await Promise.all(allArchitectNetworks.map((network) => network.remove()));
     };
 
@@ -44,8 +40,13 @@ describe("simple tests", () => {
     it(
         "Should be able to create a container without additional services",
         async () => {
-            const { container } = await architect({ withAdditionalServices: false });
+            const { container, fridaAddress } = await architect({ withAdditionalServices: false });
+            const deviceManager = frida.getDeviceManager();
+            const device = await deviceManager.addRemoteDevice(fridaAddress);
+            const processes = await device.enumerateProcesses();
             expect(container.id).toBeDefined();
+            expect(processes).toBeDefined();
+            expect(processes.length).toBeGreaterThan(0);
         },
         ARCHITECT_TEST_TIMEOUT_MS
     );
@@ -53,8 +54,39 @@ describe("simple tests", () => {
     it(
         "Should be able to create a container with additional services",
         async () => {
-            const { container } = await architect({ withAdditionalServices: false });
+            const { container, fridaAddress } = await architect({ withAdditionalServices: false });
+            const deviceManager = frida.getDeviceManager();
+            const device = await deviceManager.addRemoteDevice(fridaAddress);
+            const processes = await device.enumerateProcesses();
             expect(container.id).toBeDefined();
+            expect(processes).toBeDefined();
+            expect(processes.length).toBeGreaterThan(0);
+        },
+        ARCHITECT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        "Should be able to create a container with different port bindings",
+        async () => {
+            const { container, fridaAddress, adbConsoleAddress, adbAddress, grpcAddress } = await architect({
+                withAdditionalServices: false,
+                portBindings: {
+                    "5554/tcp": [{ HostPort: "5556" }],
+                    "5555/tcp": [{ HostPort: "5557" }],
+                    "8554/tcp": [{ HostPort: "8555" }],
+                    "27042/tcp": [{ HostPort: "27044" }],
+                },
+            });
+            const deviceManager = frida.getDeviceManager();
+            const device = await deviceManager.addRemoteDevice(fridaAddress);
+            const processes = await device.enumerateProcesses();
+            expect(container.id).toBeDefined();
+            expect(adbAddress).toContain(":5557");
+            expect(grpcAddress).toContain(":8555");
+            expect(fridaAddress).toContain(":27044");
+            expect(adbConsoleAddress).toContain(":5556");
+            expect(processes).toBeDefined();
+            expect(processes.length).toBeGreaterThan(0);
         },
         ARCHITECT_TEST_TIMEOUT_MS
     );
