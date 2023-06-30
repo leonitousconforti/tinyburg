ca9a453d588655a78eebd6ba0b7c872974bd4d72
Leo Conforti <leo@leoconforti.us>
2023-06-30 04:35:47 +0000

    update insight test

--- packages/insight/test/index.test.ts
+++ packages/insight/test/index.test.ts
@@ -1,49 +1,156 @@
-/* eslint-disable dot-notation */
-
 import type Dockerode from "dockerode";
 
 import loadApk from "@tinyburg/apks";
 import architect from "@tinyburg/architect";
 
 import { bootstrapAgentOnRemote, cleanupAgent, GetterAgents } from "../src/index.js";
+const BitbookAgent = GetterAgents["BitbookAgent"];
+const BitizenAgent = GetterAgents["BitizenAgent"];
+const CostumeAgent = GetterAgents["CostumeAgent"];
+const ElevatorAgent = GetterAgents["ElevatorAgent"];
+const FloorAgent = GetterAgents["FloorAgent"];
+const MissionAgent = GetterAgents["MissionAgent"];
+const PetAgent = GetterAgents["PetAgent"];
+const RoofAgent = GetterAgents["RoofAgent"];
 
-// Timeout tests after 1 minute and prep after 3 minute
-const INSIGHT_TEST_TIMEOUT_MS = Number.parseInt(process.env["INSIGHT_TEST_TIMEOUT_MS"] as string) || 60_000;
+// Timeout tests after 1 minute and prep after 3 minutes
+const INSIGHT_TEST_TIMEOUT_MS = Number.parseInt(process.env["INSIGHT_TEST_TIMEOUT_MS"] as string) || 1000 * 60 * 1;
 const INSIGHT_PREP_TIMEOUT_MS = Number.parseInt(process.env["INSIGHT_PREP_TIMEOUT_MS"] as string) || 1000 * 60 * 3;
 
-// Override the docker host environment variable just for these tests
-process.env["DOCKER_HOST"] = process.env["ARCHITECT_TEST_DOCKER_HOST"] || process.env["DOCKER_HOST"];
-
 describe("All getter agents should return something and not throw any errors", () => {
-    let container: Dockerode.Container;
     let fridaAddress: string;
+    let emulatorContainer: Dockerode.Container;
 
     beforeAll(async () => {
-        const apk = await loadApk("apkpure", "4.21.1");
-        const architectResult = await architect();
+        const apk = await loadApk("apkmirror", "4.22.0");
+        const architectResult = await architect({ reuseExistingContainers: false });
         await architectResult.installApk(apk);
-        container = architectResult.container;
         fridaAddress = architectResult.fridaAddress;
+        emulatorContainer = architectResult.emulatorContainer;
     }, INSIGHT_PREP_TIMEOUT_MS);
 
     afterAll(async () => {
-        await container.stop();
-        await container.remove();
+        await emulatorContainer.stop();
+        await emulatorContainer.remove();
     }, INSIGHT_PREP_TIMEOUT_MS);
 
-    for (const [agentName, agent] of Object.entries(GetterAgents)) {
-        it(
-            `${agentName} should produce data without throwing any errors`,
-            async () => {
-                const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
-                    agent,
-                    fridaAddress
-                );
-                const data = await runAgentMain();
-                expect(data).toBeDefined();
-                await cleanupAgent({ device, session, script, pid });
-            },
-            INSIGHT_TEST_TIMEOUT_MS
-        );
-    }
+    it(
+        `BitbookAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                BitbookAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `BitizenAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                BitizenAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `CostumeAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                CostumeAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `ElevatorAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                ElevatorAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `FloorAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                FloorAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `MissionAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                MissionAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `PetAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                PetAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
+
+    it(
+        `RoofAgent should produce data without throwing any errors`,
+        async () => {
+            const { device, session, script, pid, runAgentMain } = await bootstrapAgentOnRemote(
+                RoofAgent,
+                fridaAddress,
+                { compiler: "esbuild" }
+            );
+            const data = await runAgentMain();
+            expect(data).toBeDefined();
+            await cleanupAgent({ device, session, script, pid });
+        },
+        INSIGHT_TEST_TIMEOUT_MS
+    );
 });
