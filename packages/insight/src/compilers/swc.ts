e18202ded5858662210b90a45cb92f6e2be53227
Leo Conforti <leo@leoconforti.us>
2023-06-30 04:30:09 +0000

    update swc compiler for frida

--- packages/insight/src/compilers/swc.ts
+++ packages/insight/src/compilers/swc.ts
@@ -1,9 +1,8 @@
 import Debug from "debug";
-// import swc from "@swc/core";
 
 const logger: Debug.Debugger = Debug.debug("tinyburg:insight:swc-compiler");
 
 export const swcCompiler = async (agentLocation: string): Promise<string> => {
     logger(agentLocation);
-    return "";
+    throw new Error("Not implemented");
 };
