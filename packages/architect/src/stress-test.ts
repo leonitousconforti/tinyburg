64d2be66ddad581a2b4d7f9bd8fc9c0a02bb92f3
Leo Conforti <leo@leoconforti.us>
2023-06-30 04:27:15 +0000

    move architect_docker_host from tests to library

--- packages/architect/src/stress-test.ts
+++ packages/architect/src/stress-test.ts
@@ -8,9 +8,6 @@ const TOTAL_RUN_ITERATIONS: number =
 const PARALLEL_CONTAINERS_PER_RUN: number =
     Number.parseInt(process.env["ARCHITECT_STRESS_PARALLEL_CONTAINERS_PER_RUN"] as string, 10) || 3;
 
-// Override the docker host environment variable just for these tests
-process.env["DOCKER_HOST"] = process.env["ARCHITECT_DOCKER_HOST"] || process.env["DOCKER_HOST"];
-
 for (let run_index: number = 0; run_index < TOTAL_RUN_ITERATIONS; run_index++) {
     const results: Array<Awaited<ReturnType<typeof architect>>> = await Promise.all(
         Array.from({ length: PARALLEL_CONTAINERS_PER_RUN }, () => architect({ reuseExistingContainers: false }))
