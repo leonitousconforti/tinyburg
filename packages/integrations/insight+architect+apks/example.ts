b223d5988f13ec0379ad4c42c415b1f3b2fe558d
Leo Conforti <leo@leoconforti.us>
2023-07-11 04:15:56 +0000

    insight+architect+apks integration

--- packages/integrations/insight+architect+apks/example.ts
+++ packages/integrations/insight+architect+apks/example.ts
@@ -2,7 +2,7 @@ import loadApk from "@tinyburg/apks";
 import architect from "@tinyburg/architect";
 import { AllAgents, bootstrapAgentOnRemote } from "@tinyburg/insight";
 
-const apk: string = await loadApk("apkpure", "4.22.0");
+const apk: string = await loadApk("apkpure", "4.23.0");
 const { fridaAddress, installApk } = await architect({ reuseExistingContainers: true });
 await installApk(apk);
 
