947772c16f74ed071ca701a4b4dbdfcb500ab0b3
Leo Conforti <leo@leoconforti.us>
2023-07-11 04:11:39 +0000

    start explorer+architect+apks integration

---
+++ packages/integrations/explorer+architect+apks/.eslintrc.cjs
@@ -0,0 +1,22 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require("@rushstack/eslint-config/patch/modern-module-resolution");
+
+module.exports = {
+    extends: [
+        "@rushstack/eslint-config/profile/node",
+        "@rushstack/eslint-config/mixins/tsdoc",
+        "plugin:unicorn/recommended",
+        "plugin:prettier/recommended",
+    ],
+    plugins: ["unicorn", "prettier"],
+    env: {
+        node: true,
+        es2022: true,
+    },
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+        ecmaVersion: 2022,
+        sourceType: "module",
+    },
+};
---
+++ packages/integrations/explorer+architect+apks/README.md
@@ -0,0 +1,1 @@
+# @tinyburg/integrations/explorer-architect-apks
