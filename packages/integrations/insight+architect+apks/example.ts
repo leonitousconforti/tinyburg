0d34a06fa3319aa965f04d714a485e680fd6feec
Leo Conforti <leo@leoconforti.us>
2023-07-13 05:05:26 +0000

    finish integration

--- packages/integrations/insight+architect+apks/example.ts
+++ packages/integrations/insight+architect+apks/example.ts
@@ -3,9 +3,9 @@ import architect from "@tinyburg/architect";
 import { AllAgents, bootstrapAgentOnRemote } from "@tinyburg/insight";
 
 const apk: string = await loadApk("apkpure", "4.23.0");
-const { fridaAddress, installApk } = await architect({ reuseExistingContainers: true });
+const { fridaAddress, installApk } = await architect({ reuseExistingContainers: false });
 await installApk(apk);
 
-const { runAgentMain } = await bootstrapAgentOnRemote(AllAgents.BitbookAgent, fridaAddress);
-const data: string = await runAgentMain();
-console.log(data);
+const { runAgentMain } = await bootstrapAgentOnRemote(AllAgents.GoodAgent, fridaAddress);
+const data: [number, string] = await runAgentMain("world");
+console.log(`${data[0]} : ${data[1]}`);
