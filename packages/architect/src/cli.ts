0cf4ccd83d3f3d429a1c041f4ecfec4d78488aad
Leo Conforti <leo@leoconforti.us>
2023-07-16 22:32:06 +0000

    architect cli

--- packages/architect/src/cli.ts
+++ packages/architect/src/cli.ts
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-throw new Error("Not implemented yet");
+import yargs from "yargs";
+import architect from "./index.js";
+
+await yargs(process.argv.slice(2))
+    .scriptName("architect")
+    .command(
+        "$0",
+        "the default command",
+        (yargs) =>
+            yargs
+                .option("with-additional-services", { type: "boolean", default: false })
+                .option("reuse-existing-containers", { type: "boolean", default: false })
+                .options({
+                    "adb-port": { type: "string", demandOption: false, default: "0" },
+                    "grpc-port": { type: "string", demandOption: false, default: "0" },
+                    "frida-port": { type: "string", demandOption: false, default: "0" },
+                    "console-port": { type: "string", demandOption: false, default: "0" },
+                }),
+        async (argv) => {
+            await architect({
+                withAdditionalServices: argv["with-additional-services"],
+                reuseExistingContainers: argv["reuse-existing-containers"],
+                portBindings: {
+                    "5554/tcp": [{ HostPort: argv["console-port"] }],
+                    "5555/tcp": [{ HostPort: argv["adb-port"] }],
+                    "8554/tcp": [{ HostPort: argv["grpc-port"] }],
+                    "27042/tcp": [{ HostPort: argv["frida-port"] }],
+                },
+            });
+        }
+    )
+    .help()
+    .parseAsync();
