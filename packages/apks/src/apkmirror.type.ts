e2c93a2933d730b7ae6453c95d58b7476e20497f
leo conforti <leo@leoconforti.us>
2023-07-22 06:05:39 +0000

    update apks

--- packages/apks/src/apkmirror.type.ts
+++ packages/apks/src/apkmirror.type.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @rushstack/typedef-var
-export const ApkmirrorVersions = ["4.23.0"] as const;
+export const ApkmirrorVersions = ["4.23.0", "4.23.1"] as const;
 
 export type ApkmirrorVersion = (typeof ApkmirrorVersions)[number];
