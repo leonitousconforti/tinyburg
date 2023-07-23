78bce359923dbc6b09d5cf499b24577840b252bd
Leo Conforti <leo@leoconforti.us>
2023-07-23 01:28:44 +0000

    upgrade to 4.23.1

--- packages/apks/src/apkpure.type.ts
+++ packages/apks/src/apkpure.type.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @rushstack/typedef-var
-export const ApkpureVersions = ["4.23.0", "4.23.1"] as const;
+export const ApkpureVersions = ["4.23.1"] as const;
 
 export type ApkpureVersion = (typeof ApkpureVersions)[number];
