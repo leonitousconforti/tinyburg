9392e32c9166d9177d3bebd4da2cf45f5431d948
Leo Conforti <leo@leoconforti.us>
2023-07-25 01:24:31 +0000

    move emulator config.ini

--- packages/architect/emulator/launch-emulator.sh
+++ packages/architect/emulator/launch-emulator.sh
@@ -9,6 +9,9 @@ mkdir -p /root/.config/pulse
 export PULSE_SERVER=unix:/tmp/pulse-socket
 pulseaudio -D --log-time=1 --log-target=newfile:/tmp/pulseverbose.log --log-time=1 --exit-idle-time=-1
 
+# Make sure the config is in the right place
+cp /android/avd-home/config.ini /android/avd-home/Pixel2.avd/config.ini
+
 # Start the adb server and the emulator
 /android/sdk/platform-tools/adb start-server
 /android/sdk/emulator/emulator -avd Pixel2 -no-window -ports 5554,5555 -grpc 8554 -gpu swiftshader_indirect -qemu -append panic=1 &
