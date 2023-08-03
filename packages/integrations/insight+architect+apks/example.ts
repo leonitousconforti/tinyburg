16c39a1f552a19831a8767ed99243a8b284d68b1
Leo Conforti <leo@leoconforti.us>
2023-08-03 00:57:07 +0000

    fix integration

--- packages/integrations/insight+architect+apks/example.ts
+++ packages/integrations/insight+architect+apks/example.ts
@@ -2,9 +2,9 @@ import loadApk from "@tinyburg/apks";
 import architect from "@tinyburg/architect";
 import { AllAgents, bootstrapAgentOnRemote } from "@tinyburg/insight";
 
-const apk: string = await loadApk("apkpure", "4.23.0");
-const { fridaAddress, installApk } = await architect({ reuseExistingContainers: false });
-await installApk(apk);
+const apk: string = await loadApk("apkpure", "4.23.1");
+const { emulatorServices, fridaAddress } = await architect({ reuseExistingContainers: false });
+await emulatorServices.installApk(apk);
 
 const { runAgentMain } = await bootstrapAgentOnRemote(AllAgents.GoodAgent, fridaAddress);
 const data: [number, string] = await runAgentMain("world");
