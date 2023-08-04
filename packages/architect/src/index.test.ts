c58a492e6b3adab28206a23115c04bdd840b7320
Leo Conforti <leo@leoconforti.us>
2023-08-04 04:57:45 +0000

    comment out cleanup

--- packages/architect/src/index.test.ts
+++ packages/architect/src/index.test.ts
@@ -1,16 +1,16 @@
 import architect from "./index.js";
-import { cleanUpAllArchitectResources } from "./resources.js";
+// import { cleanUpAllArchitectResources } from "./resources.js";
 
 // Timeout tests after 8 minutes and cleanup after 1 minute
 const ARCHITECT_TEST_TIMEOUT_MS =
     Number.parseInt(process.env["ARCHITECT_TEST_TIMEOUT_MS"] as string, 10) || 1000 * 60 * 8;
-const ARCHITECT_CLEANUP_TIMEOUT_MS =
-    Number.parseInt(process.env["ARCHITECT_CLEANUP_TIMEOUT_MS"] as string, 10) || 60_000;
+// const ARCHITECT_CLEANUP_TIMEOUT_MS =
+//     Number.parseInt(process.env["ARCHITECT_CLEANUP_TIMEOUT_MS"] as string, 10) || 60_000;
 
 describe("simple tests", () => {
     // Yes these do actually need to be async functions and use await
-    beforeAll(async () => await cleanUpAllArchitectResources(), ARCHITECT_CLEANUP_TIMEOUT_MS);
-    afterAll(async () => await cleanUpAllArchitectResources(), ARCHITECT_CLEANUP_TIMEOUT_MS);
+    // beforeAll(async () => await cleanUpAllArchitectResources(), ARCHITECT_CLEANUP_TIMEOUT_MS);
+    // afterAll(async () => await cleanUpAllArchitectResources(), ARCHITECT_CLEANUP_TIMEOUT_MS);
 
     it(
         "Should be able to create a container without additional services",
