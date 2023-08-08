8e7ae20c29b742baa070db6a48a2a5d4c604d164
Leo Conforti <leo@leoconforti.us>
2023-08-08 00:29:13 +0000

    cleanup warnings

--- apps/architect-viewer/src/pages/Emulator.tsx
+++ apps/architect-viewer/src/pages/Emulator.tsx
@@ -1,27 +1,27 @@
 import type { IRtcClient } from "@tinyburg/architect/protobuf/rtc_service.client.js";
 import type { IEmulatorControllerClient } from "@tinyburg/architect/protobuf/emulator_controller.client.js";
 
-import React, { useState, useMemo } from "react";
-import { Alert, Box, Grid, Snackbar, Container } from "@mui/material";
+import React from "react";
+// import { Alert, Box, Grid, Snackbar, Container } from "@mui/material";
 
-import JsepProtocol from "../services/Jsep.js";
-import Copyright from "../components/Copyright.js";
-import WebRtcView from "../components/WebRtcView.js";
-import LogcatView from "../components/LogcatView.js";
-import WithMouseKeyHandler from "../components/InteractiveLayer.js";
+// import JsepProtocol from "../services/Jsep.js";
+// import Copyright from "../components/Copyright.js";
+// import WebRtcView from "../components/WebRtcView.js";
+// import LogcatView from "../components/LogcatView.js";
+// import WithMouseKeyHandler from "../components/InteractiveLayer.js";
 
 interface IEmulatorProps {
     rtcClient: IRtcClient;
     emulatorClient: IEmulatorControllerClient;
 }
 
-export const Emulator: React.FunctionComponent<IEmulatorProps> = ({ emulatorClient, rtcClient }) => {
-    const [errorMessage, setErrorMessage] = useState("");
-    const [displayErrorSnack, setDisplayErrorSnack] = useState(false);
+export const Emulator: React.FunctionComponent<IEmulatorProps> = () => {
+    // const [errorMessage, setErrorMessage] = useState("");
+    // const [displayErrorSnack, setDisplayErrorSnack] = useState(false);
 
     // const useJsep = () => useMemo(() => new JsepProtocol(rtcClient), [rtcClient]);
 
-    emulatorClient;
+    // emulatorClient;
 
     return (
         <>
