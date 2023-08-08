eb1d3ed0d5d3c2c33bd51aa326720182474d5fa4
Leo Conforti <leo@leoconforti.us>
2023-08-08 00:00:42 +0000

    move to typescript

--- packages/bots/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare type BurnBot = { playerId: string; playerSs: string };
-export declare const burnerBots: { [k: string]: BurnBot };
-export declare const getRandomBurnerBot: () => BurnBot;
--- packages/bots/index.ts
+++ packages/bots/index.ts
@@ -1,23 +1,22 @@
-export const burnerBots = {
-    BPQSY: {
+// eslint-disable-next-line @rushstack/typedef-var
+export const burnerBots = [
+    {
         playerId: "BPQSY",
         playerSs: "8dad81ae-2626-41b9-8225-325f4809057f",
     },
-    "9GV59": {
+    {
         playerId: "9GV59",
         playerSs: "be61b26e-330b-41e0-ad2f-48eb79dc3bd6",
     },
-    "9GV2Y": {
+    {
         playerId: "9GV2Y",
         playerSs: "efe5f6a3-8cd5-4956-897c-ec1db6c26485",
     },
-    "9GTYN": {
+    {
         playerId: "9GTYN",
         playerSs: "89f9b90b-4e1e-4b48-af56-df39da7b17a7",
     },
-};
+] as const;
 
-export const getRandomBurnerBot = () => {
-    const bots = Object.values(burnerBots);
-    return bots[Math.floor(Math.random() * bots.length)];
-};
+export const getRandomBurnerBot = (): (typeof burnerBots)[number] =>
+    burnerBots[Math.floor(Math.random() * burnerBots.length)]!;
