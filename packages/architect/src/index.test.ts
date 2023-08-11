5cabef34b4ca5a6a7d32d831e2ca9609a2ea7340
leo conforti <leo@leoconforti.us>
2023-08-11 02:20:32 +0000

    remove more unneeded junk

--- packages/architect/src/index.test.ts
+++ packages/architect/src/index.test.ts
@@ -2,10 +2,8 @@ import architect from "./index.js";
 import { cleanUpAllArchitectResources } from "./resources.js";
 
 // Timeout tests after 8 minutes and cleanup after 1 minute
-const ARCHITECT_TEST_TIMEOUT_MS =
-    Number.parseInt(process.env["ARCHITECT_TEST_TIMEOUT_MS"] as string, 10) || 1000 * 60 * 8;
-const ARCHITECT_CLEANUP_TIMEOUT_MS =
-    Number.parseInt(process.env["ARCHITECT_CLEANUP_TIMEOUT_MS"] as string, 10) || 60_000;
+const ARCHITECT_CLEANUP_TIMEOUT_MS = 1000 * 60;
+const ARCHITECT_TEST_TIMEOUT_MS = 1000 * 60 * 8;
 
 describe("simple tests", () => {
     // Yes these do actually need to be async functions and use await
@@ -15,11 +13,10 @@ describe("simple tests", () => {
     it(
         "Should be able to create a container without additional services",
         async () => {
-            const { emulatorServices, emulatorDataVolume } = await architect();
+            const { emulatorServices } = await architect();
             expect(emulatorServices.getEmulatorContainer().id).toBeDefined();
             await emulatorServices.stopAll();
             await emulatorServices.removeAll();
-            await emulatorDataVolume.remove();
         },
         ARCHITECT_TEST_TIMEOUT_MS
     );
--- packages/architect/src/stress-test.ts
+++ packages/architect/src/stress-test.ts
@@ -6,10 +6,8 @@ import loadApk from "@tinyburg/apks";
 const apk: string = await loadApk("apkpure", "4.24.0");
 
 // How many times to run the stress tests and how many containers to launch each time
-const TOTAL_RUN_ITERATIONS: number =
-    Number.parseInt(process.env["ARCHITECT_STRESS_TOTAL_RUN_ITERATIONS"] as string, 10) || 7;
-const PARALLEL_CONTAINERS_PER_RUN: number =
-    Number.parseInt(process.env["ARCHITECT_STRESS_PARALLEL_CONTAINERS_PER_RUN"] as string, 10) || 3;
+const TOTAL_RUN_ITERATIONS: number = 7;
+const PARALLEL_CONTAINERS_PER_RUN: number = 3;
 
 for (let run_index: number = 1; run_index <= TOTAL_RUN_ITERATIONS; run_index++) {
     const results: Array<Awaited<ReturnType<typeof architect>>> = await Promise.all(
