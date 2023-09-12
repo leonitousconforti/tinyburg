d02d121e8fe40718fb2a6118b3f0433fb88e12a0
Leo Conforti <leo@leoconforti.us>
2023-09-12 23:17:46 +0000

    no more strict mode

--- packages/spectator/src/app.tsx
+++ packages/spectator/src/app.tsx
@@ -1,12 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./app.css";
 import Emulator from "./components/Emulator.js";
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.querySelector("#root") as HTMLElement);
-root.render(
-    <React.StrictMode>
-        <Emulator address="http://10.0.0.83:32949" />
-    </React.StrictMode>
-);
+root.render(<Emulator address="http://10.0.0.83:32967" />);
