fb340ead35ffcbe4526ff334a8577a50ed56fb34
Leo Conforti <leo@leoconforti.us>
2023-09-19 04:33:41 +0000

    take address as query param

--- packages/spectator/src/app.tsx
+++ packages/spectator/src/app.tsx
@@ -4,4 +4,4 @@ import "./app.css";
 import Emulator from "./components/Emulator.js";
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.querySelector("#root") as HTMLElement);
-root.render(<Emulator address="http://10.0.0.83:32788" />);
+root.render(<Emulator address={new URLSearchParams(window.location.search).get("address") || ""} />);
