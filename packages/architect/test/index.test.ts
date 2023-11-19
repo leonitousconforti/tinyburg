e7df7090632bf6a4aba6df802ba83ec788783b60
Leo Conforti <leo@leoconforti.us>
2023-11-19 22:56:53 +0000

    remove tinyburg/archive from tinyburg/architect

--- packages/architect/test/index.test.ts
+++ packages/architect/test/index.test.ts
@@ -1,5 +1,3 @@
-import apks from "@tinyburg/archive";
-
 import architect from "../src/index.js";
 
 // Timeout tests after 14 minutes
@@ -9,10 +7,8 @@ describe("Architect tests", () => {
     it(
         "Should be able to create a container",
         async () => {
-            const apk = await apks.loadApk(apks.Games.TinyTower, "latest version");
-            const { emulatorContainer, installApk } = await architect();
+            const { emulatorContainer } = await architect();
             expect(emulatorContainer.id).toBeDefined();
-            await installApk(apk);
             await emulatorContainer.stop();
             await emulatorContainer.remove();
         },
