0df9639030e544641cd4a90c7450b11f0cff3c94
Leo Conforti <leo@leoconforti.us>
2024-05-07 21:39:37 +0000

    format

--- apps/authproxy/src/routes/v1/api.ts
+++ apps/authproxy/src/routes/v1/api.ts
@@ -6,9 +6,9 @@ import { onRequest } from "./on-request.js";
 import { preHandler } from "./pre-handler.js";
 
 // Import json schemas
-import HeadersSchema from "../schemas/headers_v1.json" assert { type: "json" };
-import ResponseSchema from "../schemas/response_v1.json" assert { type: "json" };
-import QuerystringSchema from "../schemas/querystring_v1.json" assert { type: "json" };
+import HeadersSchema from "../schemas/headers_v1.json";
+import QuerystringSchema from "../schemas/querystring_v1.json";
+import ResponseSchema from "../schemas/response_v1.json";
 
 // Import the generated interfaces
 import type { FastifyInstance } from "fastify";
