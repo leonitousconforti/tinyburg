246731be799b41031c8201cc9f2980f412b04542
Leo Conforti <leo@leoconforti.us>
2024-12-19 18:49:54 +0000

    save

--- packages/window/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import "./app.css";
-import Emulator from "./components/Emulator.js";
-
-const root: ReactDOM.Root = ReactDOM.createRoot(document.querySelector("#root") as HTMLElement);
-const address: string = new URLSearchParams(window.location.search).get("address") || "";
-console.log(address);
-root.render(<Emulator address={address} />);
