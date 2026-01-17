62e289df183d03397bf7601b031b15bbe3cec110
Leo Conforti <leo@leoconforti.us>
2026-01-17 15:16:23 -0600

    add description

--- apps/authproxy/migrations/0001_add_accounts.ts
+++ apps/authproxy/migrations/0001_add_accounts.ts
@@ -7,22 +7,23 @@ export default Effect.flatMap(
         CREATE TABLE IF NOT EXISTS accounts (
             id SERIAL PRIMARY KEY,                                  -- Auto incrementing ID
             created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),          -- Timestamp of creation
-            last_used_at TIMESTAMPTZ,                               -- Timestamp of last use
+            last_used_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),        -- Timestamp of last use
             key UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),     -- Unique account key
             revoked BOOLEAN NOT NULL DEFAULT FALSE,                 -- Revocation status
             scopes TEXT[] NOT NULL,                                 -- Permitted scopes
             rate_limit_limit INT NOT NULL,                          -- Rate limit count
-            rate_limit_window BIGINT NOT NULL                       -- Rate limit time window in milliseconds
+            rate_limit_window BIGINT NOT NULL,                      -- Rate limit time window in milliseconds
+            description TEXT                                        -- Optional description
         );
 
         -- "None" account with no permitted scopes
-        INSERT INTO accounts (key, last_used_at, rate_limit_limit, rate_limit_window, scopes)
-        VALUES ('00000000-0000-0000-0000-000000000001', NOW(), 3, (EXTRACT(EPOCH FROM INTERVAL '1 minute') * 1000)::BIGINT, ARRAY[]::TEXT[])
+        INSERT INTO accounts (key, description, rate_limit_limit, rate_limit_window, scopes)
+        VALUES ('00000000-0000-0000-0000-000000000001', 'Default None Account', 3, (EXTRACT(EPOCH FROM INTERVAL '1 minute') * 1000)::BIGINT, ARRAY[]::TEXT[])
         ON CONFLICT (key) DO NOTHING;
 
         -- "Default" account with read-only scopes
-        INSERT INTO accounts (key, last_used_at, rate_limit_limit, rate_limit_window, scopes)
-        VALUES ('00000000-0000-0000-0000-000000000002', NOW(), 3, (EXTRACT(EPOCH FROM INTERVAL '1 minute') * 1000)::BIGINT, ARRAY[
+        INSERT INTO accounts (key, description, rate_limit_limit, rate_limit_window, scopes)
+        VALUES ('00000000-0000-0000-0000-000000000002', 'Default Readonly Account', 3, (EXTRACT(EPOCH FROM INTERVAL '1 minute') * 1000)::BIGINT, ARRAY[
             '/player_details/tt/',
             '/sync/pull/tt/',
             '/sync/current_version/tt/',
