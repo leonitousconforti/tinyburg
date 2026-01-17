434d5dda845a5ae69bf386fc4a55def87ad452d4
Leo Conforti <leo@leoconforti.us>
2026-01-17 17:50:16 -0600

    add host

--- apps/authproxy/index.ts
+++ apps/authproxy/index.ts
@@ -59,6 +59,7 @@ HttpLayerRouter.serve(AllRoutes).pipe(
     Layer.provide(
         NodeHttpServer.layerConfig(createServer, {
             port: Config.number("PORT").pipe(Config.withDefault(3000)),
+            host: Config.string("HOST").pipe(Config.withDefault("0.0.0.0")),
         })
     ),
     Layer.launch,
