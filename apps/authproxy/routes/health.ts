2601ec3f751299c5beefc825a2f67ce5977ee07d
Leo Conforti <leo@leoconforti.us>
2026-01-17 22:36:25 -0600

    disable logging on the health check route

--- apps/authproxy/routes/health.ts
+++ apps/authproxy/routes/health.ts
@@ -3,17 +3,18 @@ import { NimblebitAuth } from "@tinyburg/nimblebit-sdk";
 import { TinyTower } from "@tinyburg/tinytower-sdk";
 import { Duration, Effect, Layer } from "effect";
 
-export const HealthCheck = Effect.cachedWithTTL(
-    NimblebitAuth.NimblebitAuth.pipe(
-        Effect.flatMap((auth) => auth.burnbot),
-        Effect.flatMap(TinyTower.raffle_checkEnteredCurrent),
-        Effect.as(HttpServerResponse.text("OK", { status: 200 })),
-        Effect.orDie
-    ),
-    Duration.hours(1)
-);
+export const HealthCheckRoute = Layer.unwrapEffect(
+    Effect.gen(function* () {
+        const HealthCheck = yield* NimblebitAuth.NimblebitAuth.pipe(
+            Effect.flatMap((auth) => auth.burnbot),
+            Effect.flatMap(TinyTower.raffle_checkEnteredCurrent),
+            Effect.as(HttpServerResponse.text("OK", { status: 200 })),
+            Effect.tapError(Effect.logError),
+            Effect.tapDefect(Effect.logError),
+            Effect.orDie,
+            Effect.cachedWithTTL(Duration.hours(1))
+        );
 
-export const HealthCheckRoute = HealthCheck.pipe(
-    Effect.map((execute) => HttpLayerRouter.add("GET", "/healthz", execute)),
-    Layer.unwrapEffect
-);
+        return HttpLayerRouter.add("GET", "/healthz", HealthCheck);
+    })
+).pipe(Layer.provide(HttpLayerRouter.disableLogger));
