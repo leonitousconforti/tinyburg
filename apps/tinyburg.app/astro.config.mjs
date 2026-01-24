5260aedb6778ad78f1ab88a3947b589364180044
Leo Conforti <leo@leoconforti.us>
2026-01-23 23:46:57 -0600

    fix allowed hosts

--- apps/tinyburg.app/astro.config.mjs
+++ apps/tinyburg.app/astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        server: {
-            allowedHosts: ["tinyburg.app", "www.tinyburg.app"],
-        },
+    server: {
+        allowedHosts: ["tinyburg.app", "www.tinyburg.app"],
     },
 });
