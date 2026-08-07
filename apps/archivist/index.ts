5c06dd26e7fa440659a44c1607d247535eb64a5b
Leo Conforti <leo@leoconforti.us>
2026-08-07 18:42:57 -0500

    archive all sdk-supported Nimblebit games in archivist

--- apps/archivist/index.ts
+++ apps/archivist/index.ts
@@ -4,12 +4,14 @@ import { NodeRuntime } from "@effect/platform-node";
 import { GooglePlayApi } from "@efffrida/gplayapi";
 
 import { archiveToS3 } from "./archive.ts";
+import { bundleIdentifiers } from "./games.ts";
 import { Live } from "./layers.ts";
 
 Effect.gen(function* () {
-    const bundleIdentifier = "com.nimblebit.tinytower";
-    const details = yield* GooglePlayApi.details(bundleIdentifier);
-    const versionCode = details.item?.details?.appDetails?.versionCode ?? 0n;
-    yield* Effect.logInfo(`Archiving ${bundleIdentifier} version ${versionCode}`);
-    yield* archiveToS3({ offerType: details.item?.offer[0].offerType ?? 1, versionCode });
+    for (const bundleIdentifier of bundleIdentifiers) {
+        const details = yield* GooglePlayApi.details(bundleIdentifier);
+        const versionCode = details.item?.details?.appDetails?.versionCode ?? 0n;
+        yield* Effect.logInfo(`Archiving ${bundleIdentifier} version ${versionCode}`);
+        yield* archiveToS3({ bundleIdentifier, offerType: details.item?.offer[0].offerType ?? 1, versionCode });
+    }
 }).pipe(Effect.provide(Live), NodeRuntime.runMain);
--- apps/archivist/seed.ts
+++ apps/archivist/seed.ts
@@ -4,14 +4,16 @@ import { NodeRuntime } from "@effect/platform-node";
 import { GooglePlayApi } from "@efffrida/gplayapi";
 
 import { archiveToS3 } from "./archive.ts";
+import { bundleIdentifiers } from "./games.ts";
 import { Live } from "./layers.ts";
 
 Effect.gen(function* () {
-    const bundleIdentifier = "com.nimblebit.tinytower";
-    const { item } = yield* GooglePlayApi.details(bundleIdentifier);
-    const maxVersionCode = item?.details?.appDetails?.versionCode ?? 0n;
-    for (let versionCode = BigInt(0); versionCode <= maxVersionCode; versionCode++) {
-        yield* Effect.logInfo(`Archiving ${bundleIdentifier} version ${versionCode}`);
-        yield* archiveToS3({ offerType: item?.offer[0].offerType ?? 1, versionCode });
+    for (const bundleIdentifier of bundleIdentifiers) {
+        const { item } = yield* GooglePlayApi.details(bundleIdentifier);
+        const maxVersionCode = item?.details?.appDetails?.versionCode ?? 0n;
+        for (let versionCode = BigInt(0); versionCode <= maxVersionCode; versionCode++) {
+            yield* Effect.logInfo(`Archiving ${bundleIdentifier} version ${versionCode}`);
+            yield* archiveToS3({ bundleIdentifier, offerType: item?.offer[0].offerType ?? 1, versionCode });
+        }
     }
 }).pipe(Effect.provide(Live), NodeRuntime.runMain);
