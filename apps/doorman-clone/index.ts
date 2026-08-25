df6e8b952dc40b81bd062d41dd419e88bc313ce1
Leo Conforti <leo@leoconforti.us>
2026-08-25 13:56:57 -0500

    fix doorman clone config provider

--- apps/doorman-clone/index.ts
+++ apps/doorman-clone/index.ts
@@ -5,19 +5,14 @@ import { NodeRuntime, NodeServices } from "@effect/platform-node";
 import { NimblebitAuth, NimblebitConfig } from "@tinyburg/nimblebit-sdk";
 import { Bitizens, SyncItemType, TinyTower } from "@tinyburg/tinytower-sdk";
 
-const DotEnvLive = Effect.map(ConfigProvider.fromDotEnv(), ConfigProvider.nested("DOORMANCLONE"));
-const ConfigLive = ConfigProvider.nested(ConfigProvider.fromEnv(), "DOORMANCLONE");
+const ConfigProviderLive = ConfigProvider.fromEnv().pipe(ConfigProvider.nested("DOORMANCLONE"), ConfigProvider.layer);
 
 const Live = Layer.merge(
     FetchHttpClient.layer,
     NimblebitAuth.layerTinyburgAuthProxyConfig({
         authKey: Config.redacted("AUTHPROXY_AUTH_KEY"),
     })
-).pipe(
-    Layer.provideMerge(ConfigProvider.layer(ConfigLive)),
-    Layer.provideMerge(ConfigProvider.layerAdd(DotEnvLive)),
-    Layer.provide(NodeServices.layer)
-);
+).pipe(Layer.provideMerge(ConfigProviderLive), Layer.provide(NodeServices.layer));
 
 const program = Effect.gen(function* () {
     const authenticatedPlayer = yield* NimblebitConfig.AuthenticatedPlayerConfig;
